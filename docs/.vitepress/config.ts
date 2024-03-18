5869b8f30921218dd02b059c5fdb48801aa0752a
yvan <luoyifan@qq.com>
2024-03-18 15:24:44 +0800

    init

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -145,6 +145,30 @@ export default defineConfig({
         lineNumbers: true,
         config: (md) => {
             md.use(timeline);
+            md.use((md) => {
+                let defaultRender = md.renderer.rules.text;
+                let inLineRender = md.renderer.rules.html_inline;
+
+                md.renderer.rules.text = (tokens, idx, options, env, self) => {
+                    const token = tokens[idx];
+
+                    // obsidian 图片格式转换
+                    // ![[Pasted image 20240315152420.png]]
+                    // 转换为
+                    // ![](./_assets/Pasted%20image%2020240315152420.png)
+                    if (token.type === 'text' && token.content.indexOf('![[') >= 0 && token.content.indexOf(']]') >= 0) {
+                        const item = token.content;
+                        const imgName = item.substring(3, item.length - 2)
+                            .replace(/ /g, '%20')
+                        // console.log('imgName =', imgName)
+
+                        tokens[idx].content = `<img src="./_assets/${imgName}" alt="" loading="lazy">`
+                        return inLineRender(tokens, idx, options, env, self);
+                    }
+
+                    return defaultRender(tokens, idx, options, env, self);
+                }
+            })
         },
         image: {
             lazyLoading: true
